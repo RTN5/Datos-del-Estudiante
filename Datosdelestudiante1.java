39e3efd861a3603ec163de59392f221d304d28ef
RTN5 <rt5scrp@outlook.com>
2020-05-27 23:10:26 -0500

    information

---
+++ Datosdelestudiante1.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datosdelestudiante1;
+
+/**
+ *
+ * @author User
+ */
+import java.util.Scanner;
+public class Datosdelestudiante1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Scanner objetivo=new Scanner(System.in);
+        System.out.println("Ingrese nombre de la Universidad:");
+        String nombreUniversidad = objetivo.nextLine();
+        
+        System.out.println("Ingrese departamento:");
+        String departamento=objetivo.nextLine();
+        
+        System.out.println("Ingrese la carrera:");
+        String carrera=objetivo.nextLine();
+        
+        
+        System.out.println("Ingrese la materia:");
+        String materia=objetivo.nextLine();
+        
+     
+        System.out.println("Ingrese nombres del estudiante:");
+        String nombreestudiante=objetivo.nextLine();
+        
+        System.out.println("Ingrese nivel educativo:");
+        String niveleducativo=objetivo.nextLine();
+        
+        System.out.println("Ingrese numero de cedla:");
+        String numerocedula=objetivo.nextLine();
+        
+        
+        System.out.println("Ingrese pais:");
+        String pais=objetivo.nextLine();
+        
+        System.out.println("Ingrese ciudad:");
+        String ciudad=objetivo.nextLine();
+        
+        System.out.println("Ingrese canton:");
+        String canton=objetivo.nextLine();
+        
+        System.out.println("Ingrese provincia:");
+        String provincia=objetivo.nextLine();
+        
+        System.out.println("Ingrese ID");
+        String ID=objetivo.nextLine();
+        
+        System.out.println("Ingrese sexo:");
+        String sexo=objetivo.nextLine();
+
+        
+    }
+    
+}
